9be19548625dc7217235e477690e9e2cbebb6b4f
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-05-17 22:00:11 +0200

    add main app & include styles

---
+++ main/views.py
@@ -0,0 +1,10 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request: WSGIRequest) -> HttpResponse:
+
+    return render(request, 'main/index.html')
