747d4480bcc019d68e7314a8ff0a09b579c15768
rubix0307 <miroshnichenkoartem0307@gmail.com>
2024-06-20 12:38:42 +0200

    separate payments on fee and service

--- main/views.py
+++ main/views.py
@@ -39,20 +39,22 @@ def get_payments(
         visa_quotas_price_total_start_value=None,
         visa_now_price_total=None,
         visa_now_price_total_start_value=None,
+        visa_now_services=None,
+        visa_now_services_start_value=None,
         private_shareholders_price_total=None,
         private_shareholders_price_total_start_value=None,
         legal_shareholders_price_total=None,
         legal_shareholders_price_total_start_value=None,
         bank_account_registration_service=None,
         bank_account_registration_service_start_value=None,
-        office_price=None,
+        office_price_total=None,
         office_price_start_value=None,
         office_search_service=None,
         office_search_service_start_value=None,
         company_registration=None,
         company_registration_start_value=None,
-        professional_company_registration_service=None,
-        professional_company_registration_service_start_value=None
+        registration_service=None,
+        registration_service_start_value=None
     ):
 
     payments = []
@@ -75,6 +77,12 @@ def get_payments(
             value=visa_now_price_total,
             is_start_value=visa_now_price_total_start_value,
         ))
+    if visa_now_services:
+        payments.append(TempPriceDataView(
+            description='Услуга по выпуску виз',
+            value=visa_now_services,
+            is_start_value=visa_now_services_start_value,
+        ))
     if private_shareholders_price_total:
         payments.append(TempPriceDataView(
             description='Регистрация всех акционеров физ. лиц',
@@ -93,10 +101,10 @@ def get_payments(
             value=bank_account_registration_service,
             is_start_value=bank_account_registration_service_start_value,
         ))
-    if office_price:
+    if office_price_total:
         payments.append(TempPriceDataView(
             description='Аренда офиса',
-            value=office_price,
+            value=office_price_total,
             is_start_value=office_price_start_value,
         ))
     if office_search_service:
@@ -113,11 +121,11 @@ def get_payments(
             is_start_value=company_registration_start_value,
         ))
 
-    if professional_company_registration_service:
+    if registration_service:
         payments.append(TempPriceDataView(
             description='Профессиональные услуги',
-            value=professional_company_registration_service,
-            is_start_value=professional_company_registration_service_start_value,
+            value=registration_service,
+            is_start_value=registration_service_start_value,
         ))
 
     return payments
@@ -176,29 +184,26 @@ class FormData:
         self.email: str = self.post.get('email')
 
         self.bank_account: Literal['company', 'personal', 'no'] = self.post.get('bank_account')
-        if self.bank_account == 'company':
+        self.contract_of_residence: Literal['self', 'minimal'] = self.post.get('contract_of_residence')
 
 
-            self.contract_of_residence: Literal['self', 'minimal'] = self.post.get('contract_of_residence')
+        self.bank_name = self.post.get('bank_name')
+        self.bank_currency = self.post.get('bank_currency')
+        self.bank_month_activity_input_min = self.post.get('bank_month_activity_input_min')
+        self.bank_month_activity_input_max = self.post.get('bank_month_activity_input_max')
+        self.bank_month_activity_output_min = self.post.get('bank_month_activity_output_min')
+        self.bank_month_activity_output_max = self.post.get('bank_month_activity_output_max')
 
+        self.bank_minimal_monthly_balance = self.post.get('bank_minimal_monthly_balance')
 
-            self.bank_name = self.post.get('bank_name')
-            self.bank_currency = self.post.get('bank_currency')
-            self.bank_month_activity_input_min = self.post.get('bank_month_activity_input_min')
-            self.bank_month_activity_input_max = self.post.get('bank_month_activity_input_max')
-            self.bank_month_activity_output_min = self.post.get('bank_month_activity_output_min')
-            self.bank_month_activity_output_max = self.post.get('bank_month_activity_output_max')
+        self.bank_minimal_monthly_custom_balance = self.post.get('bank_minimal_monthly_custom_balance')
+        self.bank_minimal_monthly_balance = self.bank_minimal_monthly_custom_balance
 
-            self.bank_minimal_monthly_balance = self.post.get('bank_minimal_monthly_balance')
-            if self.bank_minimal_monthly_balance == 'custom':
-                self.bank_minimal_monthly_custom_balance = self.post.get('bank_minimal_monthly_custom_balance')
-                self.bank_minimal_monthly_balance = self.bank_minimal_monthly_custom_balance
+        self.source_of_funds = self.post.get('source_of_funds')
 
-            self.source_of_funds = self.post.get('source_of_funds')
-
-            self.company_annual_turnover_this_year = self.post.get('company_annual_turnover_this_year')
-            self.company_annual_turnover_next_year = self.post.get('company_annual_turnover_next_year')
-            self.company_annual_turnover_from_two_years = self.post.get('company_annual_turnover_from_two_years')
+        self.company_annual_turnover_this_year = self.post.get('company_annual_turnover_this_year')
+        self.company_annual_turnover_next_year = self.post.get('company_annual_turnover_next_year')
+        self.company_annual_turnover_from_two_years = self.post.get('company_annual_turnover_from_two_years')
 
     def parse_activities(self):
         return [Activity(name=activity_name,specializations=self.body.get(f'specialization_{activity_name}')) for activity_name in self.body.get('activities', [])]
@@ -300,7 +305,7 @@ class PriceData:
             ),
         )
 
-    def offshore(self):
+    def offshore(self) -> Solution:
         activities_price_total, activities_cost_price_total = self.calculate_activities(free_count=3, price=1000)
         bank_account_registration_service = self.calculate_bank_account_registration_service()
 
@@ -326,14 +331,13 @@ class PriceData:
         legal_shareholders_cost_price_total = legal_shareholders_price_total
 
         company_registration = 3000 * 1.05
-        professional_company_registration_service = 6500
+        registration_service = 6500
 
         cost_price_total = sum([
             activities_cost_price_total,
             private_shareholders_cost_price_total,
             legal_shareholders_cost_price_total,
             company_registration,
-
         ])
         price_total = sum([
             activities_price_total,
@@ -341,7 +345,7 @@ class PriceData:
             private_shareholders_price_total,
             legal_shareholders_price_total,
             company_registration,
-            professional_company_registration_service,
+            registration_service,
         ])
         payments = get_payments(
             activities_price_total=activities_price_total,
@@ -349,7 +353,7 @@ class PriceData:
             private_shareholders_price_total=private_shareholders_price_total,
             legal_shareholders_price_total=legal_shareholders_price_total,
             company_registration=company_registration,
-            professional_company_registration_service=professional_company_registration_service,
+            registration_service=registration_service,
         )
 
         unavailable = [
@@ -369,12 +373,12 @@ class PriceData:
             unavailable=unavailable,
         )
 
-    def mainland(self):
+    def mainland(self) -> Solution:
         activities_count = len(self.data.activities)
         specializations_count = len(sum([a.specializations for a in self.data.activities], []))
         free_specializations_count = 10
-        activity_cost_price = 20000 # TODO price start with
-        activities_price_total = activity_cost_price # TODO price start with
+        activity_cost_price = 20000
+        activities_price_total = activity_cost_price
 
         need_companies = False
         if activities_count > 1:
@@ -387,26 +391,28 @@ class PriceData:
 
         # visa now
         visa_cost_price = 5200 * 1.05
-        visa_price = 5500
+        visa_professional_services = 5500
         visa_now_cost_price_total = visa_cost_price * self.data.visa_quotas_now
-        visa_now_price_total = (visa_cost_price + visa_price) * self.data.visa_quotas_now
-
+        visa_now_price_total = visa_cost_price * self.data.visa_quotas_now
+        visa_now_services = visa_professional_services * self.data.visa_quotas_now
 
         office_cost_price = 0
-        office_price = 0
+        office_price_total = 0
         office_search_service = 0
+        office_price_start_value = False
         if self.data.office == 'real':
-            office_cost_price = 25000  # TODO price start with
-            office_price = office_cost_price
+            office_cost_price = 25000
+            office_price_total = office_cost_price
+            office_price_start_value = True
             office_search_service = 4000
 
         elif self.data.office == 'minimal':
             office_cost_price = 10000
-            office_price = office_cost_price
+            office_price_total = office_cost_price
             office_search_service = 0
 
         # professional services
-        professional_company_registration_service = 8000
+        registration_service = 8000
 
         cost_price_total = sum([
             activity_cost_price,
@@ -417,20 +423,23 @@ class PriceData:
         price_total = sum([
             activities_price_total,
             visa_now_price_total,
-            bank_account_registration_service,
-            office_price,
+            visa_now_services,
+            office_price_total,
             office_search_service,
-            professional_company_registration_service,
+            bank_account_registration_service,
+            registration_service,
         ])
         payments = get_payments(
             activities_price_total=activities_price_total,
             activities_price_total_start_value=True,
             visa_now_price_total=visa_now_price_total,
-            office_price=office_price,
-            office_price_start_value=True,
+            visa_now_price_total_start_value=True,
+            visa_now_services=visa_now_services,
+            office_price_total=office_price_total,
+            office_price_start_value=office_price_start_value,
             office_search_service=office_search_service,
             bank_account_registration_service=bank_account_registration_service,
-            professional_company_registration_service=professional_company_registration_service,
+            registration_service=registration_service,
         )
 
         unavailable = [
@@ -452,12 +461,15 @@ class PriceData:
 
 
 
-    def ifza(self):
+    def ifza(self) -> Solution:
         activities_price_total, activities_cost_price_total = self.calculate_activities(free_count=3, price=1000)
         bank_account_registration_service = self.calculate_bank_account_registration_service()
 
 
         # visa quotas
+        establishment_card_cost_price = 2000
+        establishment_card_price_total = establishment_card_cost_price
+
         quotas_count = self.data.visa_quotas
         quotas_data = {
             0: {'price': 12900, 'cost_price': 10300},
@@ -470,8 +482,8 @@ class PriceData:
         if quotas_count > max_key:
             quotas_count = max_key
 
-        visa_quotas_price_total = quotas_data[quotas_count]['price']
-        visa_quotas_cost_price_total = quotas_data[quotas_count]['cost_price']
+        visa_quotas_price_total = quotas_data[quotas_count]['price'] +  establishment_card_price_total
+        visa_quotas_cost_price_total = quotas_data[quotas_count]['cost_price'] + establishment_card_cost_price
 
 
         # visa now
@@ -481,7 +493,8 @@ class PriceData:
         residence_medial_price = 500
 
         visa_now_cost_price_total = (residence_visa_fee + residence_id_price + residence_medial_price) * self.data.visa_quotas_now
-        visa_now_price_total = visa_now_cost_price_total + (visa_professional_services * self.data.visa_quotas_now)
+        visa_now_price_total = visa_now_cost_price_total
+        visa_now_services = visa_professional_services * self.data.visa_quotas_now
 
         # private_shareholder
         free_private_shareholder_count = 3
@@ -489,7 +502,7 @@ class PriceData:
 
         private_shareholders_price_total = 0
         if self.data.private_shareholders_count > free_private_shareholder_count:
-            private_shareholder_price_total = (self.data.private_shareholders_count - free_private_shareholder_count) * private_shareholder_price
+            private_shareholders_price_total = (self.data.private_shareholders_count - free_private_shareholder_count) * private_shareholder_price
         private_shareholder_cost_price_total = private_shareholders_price_total
 
 
@@ -500,24 +513,27 @@ class PriceData:
 
 
         office_cost_price = 0
-        office_price = 0
+        office_price_total = 0
         office_search_service = 0
-        office_is_start_price = False
+        office_price_start_value = False
 
         if self.data.office == 'real':
             office_cost_price = 25000
-            office_price = office_cost_price
+            office_price_total = office_cost_price
+            office_price_start_value = True
             office_search_service = 3000
-            office_is_start_price = True
 
         elif self.data.office == 'minimal':
             office_cost_price = 13500
-            office_price = office_cost_price
+            office_price_total = office_cost_price
             office_search_service = 0
 
 
         # professional services
-        professional_company_registration_service = 6600
+        registration_service = 6600
+
+
+
 
         cost_price_total = sum([
             activities_cost_price_total,
@@ -526,6 +542,7 @@ class PriceData:
             private_shareholder_cost_price_total,
             legal_shareholders_cost_price_total,
             office_cost_price,
+            establishment_card_cost_price,
         ])
         price_total = sum([
             activities_price_total,
@@ -533,23 +550,26 @@ class PriceData:
             visa_now_price_total,
             private_shareholders_price_total,
             legal_shareholders_price_total,
+            office_price_total,
+            visa_now_services,
             bank_account_registration_service,
-            office_price,
             office_search_service,
-            professional_company_registration_service,
+            registration_service,
+            establishment_card_price_total,
         ])
 
         payments = get_payments(
             activities_price_total=activities_price_total,
             visa_quotas_price_total=visa_quotas_price_total,
             visa_now_price_total=visa_now_price_total,
+            visa_now_services=visa_now_services,
             private_shareholders_price_total=private_shareholders_price_total,
             legal_shareholders_price_total=legal_shareholders_price_total,
             bank_account_registration_service=bank_account_registration_service,
-            office_price=office_price,
-            office_price_start_value=True,
+            office_price_total=office_price_total,
+            office_price_start_value=office_price_start_value,
             office_search_service=office_search_service,
-            professional_company_registration_service=professional_company_registration_service,
+            registration_service=registration_service,
         )
         unavailable = []
 
@@ -595,7 +615,7 @@ class PriceData:
             return 5509.5, f'Номинальный сервис (секретарь, {"акционер" if shareholder else "директор"})'
 
         if shareholder and director:
-            return 9182.5, 'Номинальный сервис (акционер, директор)'
+            return 7346, 'Номинальный сервис (акционер, директор)'
 
         cost = 0
         description = 'Номинальный сервис '
